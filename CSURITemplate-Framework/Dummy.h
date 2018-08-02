c150c8b4c5afc103ac3c2d690fc082a9933cc43e
Vitor Magalhães <vitor.magalhaes@ndrive.com>
2018-08-02 17:53:23 +0100

    added dummy file to force static binary link

---
+++ CSURITemplate-Framework/Dummy.h
@@ -0,0 +1,13 @@
+//
+//  Dummy.h
+//  CSURITemplate
+//
+//  Created by Vitor Magalhães on 02/08/2018.
+//  Copyright © 2018 Cogenta Systems Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dummy : NSObject
+
+@end
---
+++ CSURITemplate-Framework/Dummy.m
@@ -0,0 +1,15 @@
+//
+//  Dummy.m
+//  CSURITemplate
+//
+//  Created by Vitor Magalhães on 02/08/2018.
+//  Copyright © 2018 Cogenta Systems Ltd. All rights reserved.
+//
+
+#import "Dummy.h"
+
+#import "CSURITemplate.h"
+
+@implementation Dummy
+
+@end
